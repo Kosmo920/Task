ea3ac82df2e660b3f775da477824ce17074d52ff
Kosmo9200 <kosmo9200@gmail.com>
2024-06-03 21:33:42 +0300

    1.2

--- js.js
+++ js.js
@@ -1,9 +1,21 @@
-const url = "https://rwl.artport.pro/commercialAgent/hs/CarrWorkApp/VagonInfo"
+const url = "https://rwl.artport.pro/commercialAgent/hs/CarrWorkApp/VagonInfo";
 
 async function fetchVagons(){
     const response = await fetch(url);
     const {Vagons} = await response.json();
-    console.log(Vagons);
+
+    const vagonCards = Vagons.map(vagon => {
+        const vagonCard = document.createElement('div');
+        vagonCard.innerHTML = 
+        "<p>Vagon Number:" + vagon.VagonNumber + "</p><p>Vagon Type:" + vagon.VagonType + "</p><p>Cargo Name:" + vagon.CargoName + "</p><p>Owner Name:" + vagon.OwnerName + "</p><p>Departure Station Name:" + vagon.DepartureStationName +"</p>";
+        return vagonCard;
+    });
+
+    const card = document.getElementById('cards');
+
+    vagonCards.forEach(vagonCard => {
+        card.appendChild(vagonCard);
+    })
 }
 
-fetchVagons()
+fetchVagons();
