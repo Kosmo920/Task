009a6ba62d5164a1fda1b6be63bae182d8ea79fe
Kosmo9200 <kosmo9200@gmail.com>
2024-06-04 06:27:03 +0300

    1.4

--- js.js
+++ js.js
@@ -1,17 +1,17 @@
 const url = "https://rwl.artport.pro/commercialAgent/hs/CarrWorkApp/VagonInfo";
 
-async function fetchVagons(){
+async function fetchVagons() {
     const response = await fetch(url);
-    const {Vagons} = await response.json();
+    const { Vagons } = await response.json();
     return Vagons;
 }
 
-async function displayVagons(vagons){
+async function displayVagons(vagons) {
     const card = document.getElementById('cards');
     card.innerHTML = '';
     const vagonCards = vagons.map(vagon => {
         const vagonCard = document.createElement('div');
-        vagonCard.innerHTML = 
+        vagonCard.innerHTML =
             "<p>Vagon Number: " + vagon.VagonNumber + "</p>" +
             "<p>Vagon Type: " + vagon.VagonType + "</p>" +
             "<p>Cargo Name: " + vagon.CargoName + "</p>" +
@@ -19,22 +19,31 @@ async function displayVagons(vagons){
             "<p>Departure Station Name: " + vagon.DepartureStationName + "</p>";
         return vagonCard;
     });
-    
+
     vagonCards.forEach(vagonCard => {
         card.appendChild(vagonCard);
     });
 }
 
-async function sortByNumber(){
+async function sortByNumber() {
     const vagons = await fetchVagons();
     vagons.sort((a, b) => a.VagonNumber - b.VagonNumber);
     displayVagons(vagons);
 }
 
-async function sortByStation(){
+async function sortByStation() {
     const vagons = await fetchVagons();
     vagons.sort((a, b) => a.DepartureStationName.localeCompare(b.DepartureStationName));
     displayVagons(vagons);
+}
+
+async function search() {
+    const input = document.getElementById('filterInput');
+    const searchValue = input.value;
+    const vagons = await fetchVagons();
+    const filteredVagons = vagons.filter(vagon => vagon.VagonNumber.includes(searchValue));
+
+    displayVagons(filteredVagons);
 }
 
 fetchVagons().then(displayVagons);
